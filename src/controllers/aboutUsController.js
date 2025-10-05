c5531aaf0b86d39d70ebcbc3dc4bf2c38f525574
Samson <sams1307wolde@gmail.com>
2025-10-05 22:14:00 +0300

    Defined CRUD controllers for about-us content

---
+++ src/controllers/aboutUsController.js
@@ -0,0 +1,50 @@
+const AboutUsContent = require("../models/AboutUsContent");
+
+exports.getContent = async (req, res) => {
+  try {
+    const content = await AboutUsContent.findOne().lean();
+    res.json(content);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch about us content" });
+  }
+};
+
+exports.updateAll = async (req, res) => {
+  const update = req.body;
+  const result = await AboutUsContent.updateOne({}, update, { upsert: true });
+  res.json({ message: "Creation succussful!", result });
+};
+
+exports.updateSection = async (req, res) => {
+  const { section } = req.params;
+  const update = req.body[section];
+
+  const allowedSections = ["headTitle", "mission", "values", "quote", "team"];
+
+  if (!allowedSections.includes(section)) {
+    return res.status(400).json({ error: "Invalid section" });
+  }
+
+  try {
+    const result = await AboutUsContent.updateOne(
+      {},
+      { $set: { [section]: update } },
+      { upsert: true }
+    );
+
+    const message = result.upsertedCount
+      ? `${section} section created successfully`
+      : `${section} section updated successfully`;
+
+    res.status(201).json({ message });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to update section: ", message: err.message });
+  }
+};
+
+exports.resetContent = async (req, res) => {
+  await AboutUsContent.deleteMany({});
+  res.json({ message: "About Us page's content reset" });
+};
