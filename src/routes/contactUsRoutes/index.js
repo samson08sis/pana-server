fa9962911e1014788edc2c8ac978f81b3ba8a956
Samson <sams1307wolde@gmail.com>
2025-10-10 09:58:50 +0300

    Called route in the app router

--- src/routes/contactUsRoutes/index.js
+++ src/routes/contactUsRoutes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const contactController = require("../controllers/contactController");
+const contactController = require("../../controllers/contactUsControllers");
 
 router.get("/", contactController.getContactContent);
 router.put("/update", contactController.updateAll);
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,16 @@ const router = express.Router();
 
 const homeRoutes = require("./homeRoutes");
 const aboutUsRoutes = require("./aboutUsRoutes");
+const contactUsRoutes = require("./contactUsRoutes");
 const imageRoutes = require("./couldinary/imageRoutes");
+const newsRoutes = require("./newsRoutes");
 
 // Mount each route under its own path
 router.use("/api/home", homeRoutes);
 router.use("/api/about-us", aboutUsRoutes);
+router.use("/api/contact", contactUsRoutes);
 router.use("/api/cloudinary", imageRoutes);
+router.use("/api/news", newsRoutes);
 
 // Always keep it below the other routes so requests will only match this if found no other matching routes
 router.use("/", (req, res) => {
