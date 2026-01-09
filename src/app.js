138fbe5991dc61a7bde5238a57b1f9c48d17855c
Samson <sams1307wolde@gmail.com>
2026-01-10 00:55:52 +0300

    Add a request logger middleware

--- src/app.js
+++ src/app.js
@@ -16,6 +16,11 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+app.use((req, res, next) => {
+  console.log("Incoming request:", req.method, req.url);
+  next();
+});
+
 // Routes
 app.use("/", routes);
 
