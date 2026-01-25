02ccf412c5a81740c07342c6982ffbfdf0453d70
Samson <sams1307wolde@gmail.com>
2026-01-25 10:56:44 +0300

    Define page content & data schema for services page

---
+++ src/models/Services.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const { ImageSchema, PageHeader } = require("./CommonSchemas");
+
+const ServicesPageContentSchema = new mongoose.Schema({
+  header: PageHeader,
+});
+
+const ServicesDataSchema = new mongoose.Schema({
+  title: String,
+  image: ImageSchema,
+  description: [String],
+  offerings: [String],
+});
+
+module.exports = {
+  ServicesPageContent: mongoose.model(
+    "ServicesPageContent",
+    ServicesPageContentSchema
+  ),
+  ServicesData: mongoose.model("ServicesData", ServicesDataSchema),
+};
